7218b79c534b891d26d9fd0eb622c8f396198ef9
luisluft <luberluft@gmail.com>
2021-09-11 19:05:30 -0400

    implement the list and the button features

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import data from "./data";
 import List from "./List";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  console.log("people :", people);
+
   return (
     <main>
       <section className="container">
-        <h3>0 birthdays today</h3>
-        <List></List>
-        <button onClick={() => console.log("you clicked me")}>clear all</button>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people}></List>
+        <button onClick={() => setPeople([])}>clear all</button>
       </section>
     </main>
   );
