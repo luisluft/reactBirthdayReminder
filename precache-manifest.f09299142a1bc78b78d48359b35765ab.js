8588512a467f701a3697e67559e96d8727196689
luisluft <luberluft@gmail.com>
2021-09-13 07:33:14 -0400

    Updates

---
+++ precache-manifest.f09299142a1bc78b78d48359b35765ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d492de1d5a2a46a6091f91008664e22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4d5b49c69bec3ca6909",
+    "url": "/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "a9126082705d5b5d431b",
+    "url": "/static/js/2.daefb4dd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.daefb4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d5b49c69bec3ca6909",
+    "url": "/static/js/main.5f06bc09.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
